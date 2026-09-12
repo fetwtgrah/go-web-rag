78ec4a025ccc7cc332b17d37912b53885ffe367e
Fresh <3483057204@qq.com>
2026-09-12 23:55:53 +0800

    refactor:重构了spliter的代码使其返回json字典格式

--- python-server/spliter.py
+++ python-server/spliter.py
@@ -17,5 +17,20 @@ def split_md(path: str):
                chunks.append(
                       "\n".join(current_chunks)
                )
-        return chunks
+        result=[]
+        for i,chunk in enumerate(chunks,start=1):
+               item={
+                      "id":i,
+                      "chunk":chunk
+               }
+               result.append(item)
+        return result
+                    
+#用于测试split_md   
+# if __name__ == "__main__":
 
+#     result = split_md("test.md")
+
+#     for item in result:
+#      print(item)
+#      print("\n")
--- python-server/test.py
+++
@@ -1,3 +0,0 @@
-import spliter
-chunks=spliter.split_md("test.md")
-print(chunks)
